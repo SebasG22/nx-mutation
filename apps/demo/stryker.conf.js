ad9eb93dc7e5e99fb52bde18001dd21914f3434c
Sebastian Guevara <rlxsebas@gmail.com>
2019-08-31 11:29:06 -0500

    feat(nx): using stryker on a monorepo with stryker-cli

---
+++ apps/demo/stryker.conf.js
@@ -0,0 +1,21 @@
+module.exports = function(config) {
+  config.set({
+    mutator: 'typescript',
+    packageManager: 'yarn',
+    reporters: ['html', 'clear-text', 'progress'],
+    testRunner: 'command',
+    jest: {
+      config: require('../../jest.config'),
+      projectType: 'angular-cli'
+    },
+    transpilers: [],
+    coverageAnalysis: 'off',
+    tsconfigFile: 'tsconfig.json',
+    mutate: [
+      './src/**/*.ts',
+      '!src/**/*.spec.ts',
+      '!src/test.ts',
+      '!src/environments/*.ts'
+    ]
+  });
+};
